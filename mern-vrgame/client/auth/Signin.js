7434632e3433ecc362bf2d983af2d338a116601c
sandeep-pani <80412459+sandeep-pani@users.noreply.github.com>
2023-03-23 22:46:13 +0530

    first

---
+++ mern-vrgame/client/auth/Signin.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import Icon from "@material-ui/core/Icon";
+import { makeStyles } from "@material-ui/core/styles";
+import auth from "./../auth/auth-helper";
+import { Redirect } from "react-router-dom";
+import { signin } from "./api-auth.js";
+
+const useStyles = makeStyles((theme) => ({
+  card: {
+    maxWidth: 600,
+    margin: "auto",
+    textAlign: "center",
+    marginTop: theme.spacing(5),
+    paddingBottom: theme.spacing(2),
+  },
+  error: {
+    verticalAlign: "middle",
+  },
+  title: {
+    marginTop: theme.spacing(2),
+    color: theme.palette.openTitle,
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 300,
+  },
+  submit: {
+    margin: "auto",
+    marginBottom: theme.spacing(2),
+  },
+}));
+
+export default function Signin(props) {
+  const classes = useStyles();
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    error: "",
+    redirectToReferrer: false,
+  });
+
+  const clickSubmit = () => {
+    const user = {
+      email: values.email || undefined,
+      password: values.password || undefined,
+    };
+
+    signin(user).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        auth.authenticate(data, () => {
+          setValues({ ...values, error: "", redirectToReferrer: true });
+        });
+      }
+    });
+  };
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, [name]: event.target.value });
+  };
+
+  const { from } = props.location.state || {
+    from: {
+      pathname: "/",
+    },
+  };
+  const { redirectToReferrer } = values;
+  if (redirectToReferrer) {
+    return <Redirect to={from} />;
+  }
+
+  return (
+    <Card className={classes.card}>
+      {/* 
+        fill this place with sign in code, use above functions to connect to backend
+
+      */}
+    </Card>
+  );
+}
---
+++ mern-vrgame/client/user/Signup.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import Icon from "@material-ui/core/Icon";
+import { makeStyles } from "@material-ui/core/styles";
+import { create } from "./api-user.js";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  card: {
+    maxWidth: 600,
+    margin: "auto",
+    textAlign: "center",
+    marginTop: theme.spacing(5),
+    paddingBottom: theme.spacing(2),
+  },
+  error: {
+    verticalAlign: "middle",
+  },
+  title: {
+    marginTop: theme.spacing(2),
+    color: theme.palette.openTitle,
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 300,
+  },
+  submit: {
+    margin: "auto",
+    marginBottom: theme.spacing(2),
+  },
+}));
+
+export default function Signup() {
+  const classes = useStyles();
+  const [values, setValues] = useState({
+    name: "",
+    password: "",
+    email: "",
+    open: false,
+    error: "",
+  });
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, [name]: event.target.value });
+  };
+
+  const clickSubmit = () => {
+    const user = {
+      name: values.name || undefined,
+      email: values.email || undefined,
+      password: values.password || undefined,
+    };
+    create(user).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setValues({ ...values, error: "", open: true });
+      }
+    });
+  };
+
+  return (
+    <div>
+      {/* 
+        Add signup code
+        use above handle functions to connect to backend
+      */}
+    </div>
+  );
+}
